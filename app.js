5788d5f2d1329ad49530b6dd5b8224c943b87eac
jamoldinov28 <jamoldinovvhalilillo@gmail.com>
2024-11-21 19:13:54 +0500

    masala qoshdim

--- app.js
+++ app.js
@@ -76,3 +76,27 @@
     return Math.min(...array);
   }
 }
+
+
+
+
+{
+  //Given an array of integers, return a new array with each value doubled.
+
+  function maps(x) {
+    return x.map(n => n * 2)
+  }
+  
+}
+
+
+
+
+{
+  //This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+  function simpleMultiplication(number) {
+    return number % 2 === 0 ? number * 8 : number * 9;
+  }
+  
+}
+
